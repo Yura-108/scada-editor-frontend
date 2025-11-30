061ca95634a259b18fa3a60636ec4b19e0106bf7
Yury-108 <poddybniyra12@mail.ru>
2025-11-30 13:32:27 +0300

    channels: add device deletion

--- src/app/api/device/[id]/route.ts
+++ src/app/api/device/[id]/route.ts
@@ -1,24 +1,30 @@
 import {NextRequest, NextResponse} from "next/server";
+import {cookies} from "next/headers";
 
 export async function DELETE(
     request: NextRequest,
-    { params }: { params: { id: string } }
+    { params }: { params: Promise<{ id: string }> }
 ) {
+    const cookieStore = await cookies();
+    const accessToken = cookieStore.get('access_token')?.value;
+    const {id} = await params;
 
-    const key = params.id;
+    if (!accessToken) {
+        return NextResponse.json({message: 'Unauthorized'}, {status: 401});
+    }
 
-    if (!key) {
+    if (!id) {
         return NextResponse.json({ message: 'ID обязателен' }, { status: 400 });
     }
 
-    const backendUrl = `${process.env.BACKEND_URL}/api/node/${key}`;
+    const backendUrl = `${process.env.BACKEND_URL}/api/node/${id}`;
 
     try {
         const response = await fetch(backendUrl, {
             method: 'DELETE',
             headers: {
+                Authorization: `Bearer ${accessToken}`, // ← передаём токен
                 'Content-Type': 'application/json',
-                Cookie: request.headers.get('cookie') || '',
             },
         });
 
--- src/app/api/device/route.ts
+++ src/app/api/device/route.ts
@@ -1,39 +1,39 @@
-import { NextRequest, NextResponse } from 'next/server';
+import {NextRequest, NextResponse} from 'next/server';
 import {DeviceNodeType} from "@/types/nodeTypes";
 import {cookies} from "next/headers";
 
 // Получить список всех устройств
 export async function GET(req: NextRequest) {
-    const cookieStore = await cookies();
-    const accessToken = cookieStore.get('access_token')?.value;
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get('access_token')?.value;
 
-    if (!accessToken) {
-        return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
-    }
+  if (!accessToken) {
+    return NextResponse.json({message: 'Unauthorized'}, {status: 401});
+  }
 
 
-  const { searchParams } = new URL(req.url);
+  const {searchParams} = new URL(req.url);
   const site = searchParams.get('site');
   const project = searchParams.get('project');
 
   if (!site || !project) {
-    return NextResponse.json({ message: 'Bad request' }, { status: 400 });
+    return NextResponse.json({message: 'Bad request'}, {status: 400});
   }
 
   const backendUrl = `${process.env.BACKEND_URL}/api/node/all?site=${site}&project=${project}`;
 
-    const response = await fetch(backendUrl, {
-        method: 'GET',
-        headers: {
-            Authorization: `Bearer ${accessToken}`, // ← передаём токен
-            'Content-Type': 'application/json',
-        },
-    });
+  const response = await fetch(backendUrl, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${accessToken}`, // ← передаём токен
+      'Content-Type': 'application/json',
+    },
+  });
 
-    if (!response.ok) {
-        const text = await response.text();
-        throw new Error(`Ошибка ${response.status}: ${text}`);
-    }
+  if (!response.ok) {
+    const text = await response.text();
+    throw new Error(`Ошибка ${response.status}: ${text}`);
+  }
 
   const data = await response.json().catch(() => null);
 
@@ -42,32 +42,32 @@ export async function GET(req: NextRequest) {
 
 // Создать новое устройство
 export async function POST(req: NextRequest) {
-    try {
-        const node: DeviceNodeType = await req.json();
+  try {
+    const node: DeviceNodeType = await req.json();
 
-        if (!node) {
-            return NextResponse.json(
-                { error: "Название обязательно" },
-                { status: 400 }
-            );
-        }
-
-        const backendUrl = `${process.env.BACKEND_URL}/api/node`;
-
-        const response = await fetch(backendUrl, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(node),
-        });
-
-        const newDevice = response.json();
-
-        return NextResponse.json(newDevice, {status: 201});
-    } catch (err) {
-        console.error(err);
-        return NextResponse.json(
-            { error: "Ошибка при создании устройства" },
-            { status: 500 }
-        );
+    if (!node) {
+      return NextResponse.json(
+        {error: "Название обязательно"},
+        {status: 400}
+      );
     }
+
+    const backendUrl = `${process.env.BACKEND_URL}/api/node`;
+
+    const response = await fetch(backendUrl, {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(node),
+    });
+
+    const newDevice = response.json();
+
+    return NextResponse.json(newDevice, {status: 201});
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json(
+      {error: "Ошибка при создании устройства"},
+      {status: 500}
+    );
+  }
 }
